dd1f728a97693d9d8b8d04d1d8a734e4cb0e8dbd
ryoooosk <pond.well.0205@gmail.com>
2025-08-14 22:29:09 +0900

    refactor(iam/role/oidc/github): コメント追加

--- modules/iam/role/oidc/github/role.tf
+++ modules/iam/role/oidc/github/role.tf
@@ -15,29 +15,33 @@ data "aws_iam_policy_document" "assume_role_policy" {
     effect = "Allow"
     # GitHub Actionsのsts:AssumeRoleWithWebIdentityアクションを使った、一時的な認証情報の取得を許可
     principals {
-      type = "Federated"
+      type = "Federated" # フェデレーテッド（OIDC）認証を利用
       identifiers = [
+        # AWSアカウントIDに紐づくGitHub OIDCプロバイダーARN（Amazon Resource Name）
         "arn:aws:iam::${data.aws_caller_identity.caller_identity.account_id}:oidc-provider/token.actions.githubusercontent.com"
       ]
     }
+
+    # Security Token Service（STS）が提供するアクション
     actions = [
-      "sts:AssumeRoleWithWebIdentity"
+      "sts:AssumeRoleWithWebIdentity" # 外部IDプロパイダーで認証されたユーザーやサービスが、AWSのIAMロールを一時的に引き受けるために使われる。
     ]
-    # 特定のGitHub Organization内の指定リポジトリからのアクセスのみを許可したconditionブロックを定義
+    # subクレームが指定したGitHub リポジトリに一致することを要求
     condition {
-      test     = "Stringlike"
-      variable = "token.actions.githubusercontent.com:sub"
+      test     = "Stringlike"                              # 文字列パターン一致による条件
+      variable = "token.actions.githubusercontent.com:sub" # トークンが発行されたユーザーやサービスを一意に識別するために値（OIDCトークンのsubクレーム）
       # 変数を使ってAWSへのアクセス権限を付与したいGitHub Actionsワークフローの含まれるGitHub Organizationとリポジトリを指定できるように
       values = [
-        "repo:${var.github_organization_name}/${var.github_repository_name}:*"
+        "repo:${var.github_organization_name}/${var.github_repository_name}:*" # 指定リポジトリからのアクセスのみ許可
       ]
     }
 
+    # audクレームがsts.amazonaws.comであることも要求
     condition {
-      test     = "StringEquals"
-      variable = "token.actions.githubusercontent.com:aud"
+      test     = "StringEquals"                            # 完全一致による条件
+      variable = "token.actions.githubusercontent.com:aud" # OIDCトークンが度のサービス向けに発行されたかを示す値（OIDCトークンのaudクレーム）
       values = [
-        "sts.amazonaws.com"
+        "sts.amazonaws.com" # AWS STSのみを許可
       ]
     }
   }

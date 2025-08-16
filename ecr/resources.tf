962252d4465f3b67ff044c3cf00a90c9a8629548
ryoooosk <pond.well.0205@gmail.com>
2025-08-16 19:41:00 +0900

    chore: ecrのステートを作成

---
+++ ecr/resources.tf
@@ -0,0 +1,24 @@
+locals {
+  service_name = "sample"
+  role         = "web"
+}
+
+module "ecr" {
+  source       = "../modules/ecr"
+  service_name = local.service_name
+  env          = terraform.workspace
+  role         = "web"
+}
+
+module "iam_role" {
+  source       = "../modules/iam/role/oidc/github"
+  service_name = "${local.service_name}-${local.role}"
+  env          = terraform.workspace
+
+  inline_policy_documents = {
+    "ImagePush" = module.ecr.image_push_policy_document
+  }
+
+  github_organization_name = "ryoooosk"
+  github_repository_name   = "terraform-demo"
+}
--- modules/ecr/ecr.tf
+++ modules/ecr/ecr.tf
@@ -1,6 +1,18 @@
 resource "aws_ecr_repository" "repository" {
   name                 = "${var.service_name}-${var.env}-${var.role}"
   image_tag_mutability = var.image_tag_mutability
+
+  image_scanning_configuration {
+    scan_on_push = var.image_scanning_scan_on_push_enabled
+  }
+
+  tags = merge(var.repository_additional_tags, {
+    ServiceName = var.service_name
+    Env         = var.env
+    Role        = var.role
+  })
+
+  force_delete = true
 }
 
 resource "aws_ecr_lifecycle_policy" "policy" {
